cb0622f767606dd5a92a622105d47c4c112d360b
DrCloy <alsry0712@gmail.com>
2024-06-06 17:15:21 +0900

    Add init view in main

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from recipe import Recipe
-from view import View
+from view import MainView, InitView
+
 
 if __name__ == "__main__":
-    recipe = Recipe("Cocktail")
-    view = View(recipe)
-    view.create_window()
+    init_view = InitView()
+
+    while True:
+        if init_view.get_root_directory():
+            break
+
+    root_directory = init_view.get_root_directory()
+    print(root_directory)
+    # root_directory = init_view.get_root_directory()
+
+    # recipe = Recipe(root_directory)
+    # recipe.import_recipe()
+
+    # main_view = MainView()
+    # main_view.init()
