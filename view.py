62c653b6af2d38b65636a2ddcd3f9adedea48eba
DrCloy <alsry0712@gmail.com>
2024-06-08 10:55:11 +0900

    Add frames and text area

--- view.py
+++ view.py
@@ -7,21 +7,51 @@ class MainView:
     Class to create GUI for Cocktail Recipe Application
     """
 
-    def __init__(self) -> None:
-        pass
+    # Recipe class instance
+    __recipe = None
+    __dropdown_width = None
+    __width = 900
+    __height = 600
+    __padx = 10
+    __pady = 5
+
+    def __init__(self, recipe: Recipe) -> None:
+        self.__recipe = recipe
 
     def init(self):
         """
         Function to initialize GUI
         """
+        self.__create_window()
 
     def __create_window(self):
-        window = tk.Tk()
-        window.title("Cocktail Recipe Application")
-        window.geometry("800x600")
-        window.resizable(False, False)
+        self.__root = tk.Tk()
+        self.__root.title("Cocktail Recipe Application")
+        self.__root.geometry(f"{self.__width + 2 * self.__padx}x{self.__height + 2 * 4 * self.__pady}")
+        self.__root.resizable(False, False)
 
-        window.mainloop()
+        self.__recipe_detail_frame = tk.Frame(self.__root, highlightbackground="white", highlightthickness=1, width=self.__width, height=(self.__height // 7) * 4)
+        self.__recipe_detail_frame.pack_propagate(False)
+        self.__recipe_detail_frame.pack(padx=self.__padx, pady=self.__pady, fill=tk.BOTH)
+
+        self.__recipe_detail_text = tk.Text(self.__recipe_detail_frame, wrap=tk.WORD, width=self.__width - self.__padx, height=((self.__height // 7) * 4 - 2 * self.__pady), state=tk.DISABLED)
+        self.__recipe_detail_text.place(relwidth=1, relheight=1)
+        # self.__recipe_detail_text.pack(padx=self.__padx, pady=self.__pady)
+
+        self.__recipe_detail_scrollbar = tk.Scrollbar(self.__recipe_detail_frame, command=self.__recipe_detail_text.yview)
+        self.__recipe_detail_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.__recipe_detail_text.config(yscrollcommand=self.__recipe_detail_scrollbar.set)
+
+        self.__category_select_frame = tk.Frame(self.__root, highlightbackground="white", highlightthickness=1, width=self.__width, height=self.__height // 7)
+        self.__category_select_frame.pack(padx=self.__padx, pady=self.__pady, fill=tk.BOTH, expand=True)
+
+        self.__category_manage_frame = tk.Frame(self.__root, highlightbackground="white", highlightthickness=1, width=self.__width, height=self.__height // 7)
+        self.__category_manage_frame.pack(padx=self.__padx, pady=self.__pady, fill=tk.BOTH, expand=True)
+
+        self.__control_frame = tk.Frame(self.__root, highlightbackground="white", highlightthickness=1, width=self.__width, height=self.__height // 7)
+        self.__control_frame.pack(padx=self.__padx, pady=self.__pady, fill=tk.BOTH, expand=True)
+
+        self.__root.mainloop()
 
 
 class InitView:
