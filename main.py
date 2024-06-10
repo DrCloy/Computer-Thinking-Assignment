47ac9c219fed20c776c4e44029d7ab9aada20cca
DrCloy <alsry0712@gmail.com>
2024-06-10 16:03:07 +0900

    Update main code

--- main.py
+++ main.py
@@ -6,16 +6,15 @@ if __name__ == "__main__":
     init_view = InitView()
 
     while True:
-        if init_view.get_root_directory():
-            break
-        elif not init_view.is_active():
+        if not init_view.is_active():
             exit()
+        elif init_view.get_root_directory():
+            break
 
     root_directory = init_view.get_root_directory()
     print(root_directory)
 
-    # recipe = Recipe(root_directory)
-    # recipe.import_recipe()
+    recipe = Recipe(root_directory)
 
-    # main_view = MainView()
-    # main_view.init()
+    main_view = MainView(recipe)
+    main_view.init()
