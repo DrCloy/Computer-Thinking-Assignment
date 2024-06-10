d4c0d8e71554c8ed8cda191bd7f956e4573cb619
DrCloy <alsry0712@gmail.com>
2024-06-10 18:33:53 +0900

    Add dynamic combobox

--- view.py
+++ view.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import tkinter.scrolledtext as tkst
+import tkinter.ttk as ttk
 import threading
 from recipe import Recipe
 
@@ -15,7 +16,10 @@ class MainView:
     __height = 600
     __padx = 10
     __pady = 5
-    __combobox_width = 30
+    __combobox_width = 10
+    __combobox_list = []
+    __category_selected = []
+    __add_button = None
 
     def __init__(self, recipe: Recipe) -> None:
         self.__recipe = recipe
@@ -67,36 +71,91 @@ class MainView:
         self.__control_frame.grid_columnconfigure(3, weight=1)
         self.__control_frame.grid_columnconfigure(4, weight=1)
 
-        self.__recipe_status_label = tk.Label(self.__control_frame, text="Please Import Recipe", font=("Helvetica", 20))
-        self.__recipe_status_label.grid(row=0, column=0, padx=self.__padx, pady=self.__pady, sticky="nsw")
+        self.__status_label = tk.Label(self.__control_frame, text="Loading...", font=("Helvetica", 20))
+        self.__status_label.grid(row=0, column=0, padx=self.__padx, pady=self.__pady, sticky="nsw")
 
-        self.__import_button = tk.Button(self.__control_frame, text="Import", command=self.__import_recipe)
+        self.__import_button = tk.Button(self.__control_frame, text="Import", command=self.__import_recipe, state=tk.DISABLED)
         self.__import_button.grid(row=0, column=1, padx=2, sticky="e")
 
-        self.__export_button = tk.Button(self.__control_frame, text="Export", command=self.__export_recipe)
+        self.__export_button = tk.Button(self.__control_frame, text="Export", command=self.__export_recipe, state=tk.DISABLED)
         self.__export_button.grid(row=0, column=2, padx=2, sticky="e")
 
-        self.__generate_button = tk.Button(self.__control_frame, text="Generate", command=self.__generate_recipe)
+        self.__generate_button = tk.Button(self.__control_frame, text="Generate", command=self.__generate_recipe, state=tk.DISABLED)
         self.__generate_button.grid(row=0, column=3, padx=2, sticky="e")
 
         self.__exit_button = tk.Button(self.__control_frame, text="Exit", command=self.__root.quit)
         self.__exit_button.grid(row=0, column=4, padx=2, sticky="e")
 
+        self.__root.protocol("WM_DELETE_WINDOW", self.__root.quit)
+        self.__root.after(2000, self.__escape_loading_state)
         self.__root.mainloop()
 
+    def __escape_loading_state(self):
+        self.__status_label.config(text="Import Recipe to Start")
+        self.__import_button.config(state=tk.NORMAL)
+        self.__export_button.config(state=tk.NORMAL)
+        self.__generate_button.config(state=tk.NORMAL)
+
+    def __reset_after_combobox(self, widget):
+        for combobox in self.__combobox_list[self.__combobox_list.index(widget) + 1:]:
+            combobox.destroy()
+        self.__combobox_list = self.__combobox_list[:self.__combobox_list.index(widget) + 1]
+
+        if self.__add_button:
+            self.__add_button.destroy()
+
+    def __add_combobox(self, data: dict):
+        combobox = ttk.Combobox(self.__category_select_frame, values=['--Select--'] + list(data.keys()), width=self.__combobox_width)
+        combobox.set("--Select--")
+        combobox.pack(pady=self.__pady, side=tk.LEFT)
+        combobox.bind("<<ComboboxSelected>>", lambda event: self.__on_combobox_select(event, data))
+        self.__combobox_list.append(combobox)
+
+    def __add_category(self):
+        keys = []
+        for combobox in self.__combobox_list:
+            keys.append(combobox.get())
+        self.__category_selected.append('-'.join(keys))
+
+    def __add_add_button(self):
+        self.__add_button = tk.Button(self.__category_select_frame, text="Add", command=lambda: self.__add_category())
+        self.__add_button.pack(pady=self.__pady, side=tk.LEFT)
+
+    def __on_combobox_select(self, event, data: dict):
+        selected_key = event.widget.get()
+        self.__reset_after_combobox(event.widget)
+        if selected_key == "--Select--":
+            return
+
+        next_data = data[selected_key]
+        if isinstance(next_data, dict):
+            self.__add_combobox(next_data)
+        elif isinstance(next_data, list):
+            self.__add_add_button()
+
     def __import_recipe(self):
-        self.__recipe.import_recipe()
-        self.__recipe_status_label.config(text="Created At: " + self.__recipe.get_created_time())
+        try:
+            if not self.__recipe:
+                return
+            self.__recipe.import_recipe()
+            self.__status_label.config(text="Created At: " + self.__recipe.get_created_time())
+            self.__combobox_list = []
+            self.__category_selected = []
+            self.__add_button = None
+            self.__add_combobox(self.__recipe.get_category())
+        except Exception as e:
+            print(e)
+            self.__status_label.config(text="Error")
 
     def __export_recipe(self):
         self.__recipe.export_recipe()
-        self.__recipe_status_label.config(text="Exported")
-        threading.Timer(3, lambda: self.__recipe_status_label.config(text="Created At: " + self.__recipe.get_created_time())).start()
+        self.__status_label.config(text="Exported")
+        threading.Timer(3, lambda: self.__status_label.config(text="Created At: " + self.__recipe.get_created_time())).start()
 
     def __generate_recipe(self):
         self.__recipe.generate_recipe()
-        self.__recipe_status_label.config(text="Generated")
-        threading.Timer(3, lambda: self.__recipe_status_label.config(text="Created At: " + self.__recipe.get_created_time())).start()
+        self.__status_label.config(text="Generated")
+        threading.Timer(3, lambda: self.__status_label.config(text="Created At: " + self.__recipe.get_created_time())).start()
 
 
 class InitView:
